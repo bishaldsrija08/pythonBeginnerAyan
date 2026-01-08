88393b9d4c762d83a3a24acd935db12d379d0a54
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-08 07:54:49 +0545

    Add initial content for Day 6, including various number checks and loop examples

---
+++ day_6_loops/cw_hw.py
@@ -0,0 +1,13 @@
+"""
+WAP to check whether a number is positive or negative.
+WAP to check whether a number is even or odd.
+WAP to check wheter a user is eligible to vote or not. [age>=18]
+WAP to check a number is divisible by 5 or not.
+# Loops
+WAP to print first 10 natural numbers using while loop.
+WAP to print first 10 natural numbers using for loop.
+WAP to print first 10 odd numbers using while loop.
+WAP to print first 10 odd numbers using for loop.
+WAP to print first 10 even numbers using while loop.
+WAP to print first 10 even numbers using for loop. 
+"""
--- tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-print("Total is ", total)
