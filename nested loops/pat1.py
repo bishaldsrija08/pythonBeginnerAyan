9206c7fb3d04a451a2919cc9d28cd72fee0f63e9
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-25 19:26:55 +0545

    Add additional nested loop examples and remove unused temp file

--- nested loops/pat1.py
+++ nested loops/pat1.py
@@ -64,3 +64,53 @@ for i in range(line, 0, -1):
     for j in range(1, i+1):
         print('*', end=' ')
     print() # New line after inner loop
+    
+"""
+5 4 3 2 1
+5 4 3 2
+5 4 3
+5 4
+5
+"""
+lines = 5
+for i in range(lines, 0, -1):
+    for j in range(lines, lines-i, -1):
+        print(j, end=' ')
+    print() # New line after inner loop
+
+"""
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+"""
+
+lines = 5
+for i in range(1, lines+1, 1):
+    for j in range(1, lines-i+2, 1):
+        print(j, end=' ')
+    print() # New line after inner loop
+
+"""
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+"""
+
+lines = 5
+count = 1
+for i in range(1, lines+1, 1):
+    for j in range(1, i+1, 1):
+        print(count, end=' ')
+        count = count +1
+    print() # New line after inner loop
+
+"""
+* * * * *
+*       *
+*       *
+* * * * *
+"""
