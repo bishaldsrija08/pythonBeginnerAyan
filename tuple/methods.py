87c15ede6280820a75e28dc1e544ea47a7e55578
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-03-05 07:35:44 +0545

    Add initial implementations for tuple operations including joining, looping, counting, indexing, and unpacking examples

---
+++ tuple/methods.py
@@ -0,0 +1,6 @@
+thistuple = (1, 3, 7, 7, 8, 7, 5, 4, 6, 8, 5, 6, 7, 5)
+x = thistuple.count(5) # counts the number of times 5 appears in the tuple
+print(x)
+
+y = thistuple.index(8) # returns the index of the first occurrence of 8 in the tuple
+print(y)
---
+++ tuple/unpack.py
@@ -0,0 +1,6 @@
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+# ayan = fruits[0]
+(ayan, bishal, *red) = fruits
+print(ayan)
+print(bishal)
+print(red)
