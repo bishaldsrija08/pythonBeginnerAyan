d6edc984252b1bb5bf4899e2f5cebb508dd07764
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-03-08 19:14:49 +0545

    Add initial implementations for set operations including addition, updating, and iteration examples

---
+++ set/add.py
@@ -0,0 +1,8 @@
+myset = {"apple", "banana", "cherry"}
+
+tropical = {"pineapple", "mango", "papaya"}
+myset.add("orange")
+print(myset)
+
+myset.update(tropical)
+print(myset)
---
+++ set/set.py
@@ -0,0 +1,7 @@
+myset = {"apple", "banana", "cherry"}
+print(myset)
+print(len(myset))
+print(type(myset))
+
+for x in myset:
+    print(x)
